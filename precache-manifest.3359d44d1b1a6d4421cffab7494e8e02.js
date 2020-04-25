7f35eb5a208fe6df0528d7448267c5905904ac2a
Kangbumkyu <g.kangbumkyu@gmail.com>
2020-04-25 23:10:55 +0700

    Updates

---
+++ precache-manifest.3359d44d1b1a6d4421cffab7494e8e02.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3960584f1b8d52dca87172ea23cda3dd",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "fb6514d75a3ff9a6181e",
+    "url": "/movie_app/static/css/main.71e2bd6c.chunk.css"
+  },
+  {
+    "revision": "205df940b7e78a0b52f9",
+    "url": "/movie_app/static/js/2.6bf2e978.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.6bf2e978.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb6514d75a3ff9a6181e",
+    "url": "/movie_app/static/js/main.a3056e9a.chunk.js"
+  },
+  {
+    "revision": "1bb1c6c9295ab4e1c1f2",
+    "url": "/movie_app/static/js/runtime-main.25c00a87.js"
+  }
+]);
